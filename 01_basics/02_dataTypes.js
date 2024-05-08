a530ccc92481b01e73dca1f220168dabe273c6c2
RahulSinghAswal <aswalrahul576@gmail.com>
2024-05-08 15:24:16 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code newer version
+
+// alert(3+3)
+//we are using nodejs, not browser
+
+console.log(3 + 3);
+// code readability should be high
+
+console.log("Rahul");
+
+let name = "Rahul";
+
+let age = 20;
+
+let isLoggedIn = false;
+
+let state = null;
+
+// number
+// bigint - used in trading , large website like reddit
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined  => value is not assigned now
+
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof age);
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Learning-JavaScript
 Learning JavaScript from Chai aur Code YouTube channel
+
+Documentation for JS
+
+MDN
+
+EMCA Script — [https://tc39.es/ecma262/](https://tc39.es/ecma262/)
